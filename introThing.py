56bafa2180b36d9f17c462fb2461195498a41b9d
Hyrum Taylor <jacob.taylor@tooeleschools.org>
2023-08-21 14:15:55 -0600

    first push

---
+++ introThing.py
@@ -0,0 +1,15 @@
+# Hyrum taylor
+# 8/21/2023
+# starter Program
+
+
+print("My name is Jacob Hyrum taylor")
+print("I like to read")
+print("My favorite classes are science and coding.")
+print("I am a junior")
+print("I like to play Minecraft, Zelda, Stardew, ect.")
+print("I don't like having to write things")
+print("My least favorite subject is English")
+print("I have 3 dogs")
+print("I have 4 siblings")
+print("I like this class already")
